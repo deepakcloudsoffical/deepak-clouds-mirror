2c9fd99fa7564395f64b78e7c6b5db391c0d574b
Deepak Nishad <76053688+blackdeep04@users.noreply.github.com>
2021-06-28 11:52:39 +0530

    Add files via upload

---
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -0,0 +1,112 @@
+from bot import aria2, download_dict_lock, STOP_DUPLICATE_MIRROR, TORRENT_DIRECT_LIMIT
+from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
+from bot.helper.ext_utils.bot_utils import *
+from .download_helper import DownloadHelper
+from bot.helper.mirror_utils.status_utils.aria_download_status import AriaDownloadStatus
+from bot.helper.telegram_helper.message_utils import *
+import threading
+from aria2p import API
+from time import sleep
+
+
+class AriaDownloadHelper(DownloadHelper):
+
+    def __init__(self):
+        super().__init__()
+
+    @new_thread
+    def __onDownloadStarted(self, api, gid):
+        if STOP_DUPLICATE_MIRROR or TORRENT_DIRECT_LIMIT is not None:
+            sleep(0.5)
+            dl = getDownloadByGid(gid)
+            download = api.get_download(gid)
+            
+            if STOP_DUPLICATE_MIRROR:
+                LOGGER.info(f"Checking File/Folder if already in Drive...")
+                self.name = download.name
+                sname = download.name
+                if self.listener.isTar:
+                    sname = sname + ".tar"
+                if self.listener.extract:
+                    smsg = None
+                else:
+                    gdrive = GoogleDriveHelper(None)
+                    smsg, button = gdrive.drive_list(sname)
+                if smsg:
+                    aria2.remove([download])
+                    dl.getListener().onDownloadError(f'File/Folder is already available in Drive.\n\n')
+                    sendMarkup("Here are the search results:", dl.getListener().bot, dl.getListener().update, button)
+                    return
+
+            if TORRENT_DIRECT_LIMIT is not None:
+                LOGGER.info(f"Checking File/Folder Size...")
+                sleep(1.5)
+                size = aria2.get_download(gid).total_length
+                limit = TORRENT_DIRECT_LIMIT
+                limit = limit.split(' ', maxsplit=1)
+                limitint = int(limit[0])
+                if 'GB' in limit or 'gb' in limit:
+                    if size > limitint * 1024**3:
+                        aria2.remove([download])
+                        dl.getListener().onDownloadError(f'Torrent/Direct limit is {TORRENT_DIRECT_LIMIT}.\nYour File/Folder size is {get_readable_file_size(size)}')
+                        return
+                elif 'TB' in limit or 'tb' in limit:
+                    if size > limitint * 1024**4:
+                        aria2.remove([download])
+                        dl.getListener().onDownloadError(f'Torrent/Direct limit is {TORRENT_DIRECT_LIMIT}.\nYour File/Folder size is {get_readable_file_size(size)}')
+                        return
+        update_all_messages()
+
+    def __onDownloadComplete(self, api: API, gid):
+        LOGGER.info(f"onDownloadComplete: {gid}")
+        dl = getDownloadByGid(gid)
+        download = api.get_download(gid)
+        if download.followed_by_ids:
+            new_gid = download.followed_by_ids[0]
+            new_download = api.get_download(new_gid)
+            with download_dict_lock:
+                sleep(0.5)
+                download_dict[dl.uid()] = AriaDownloadStatus(new_gid, dl.getListener())
+                if new_download.is_torrent:
+                    download_dict[dl.uid()].is_torrent = True
+            update_all_messages()
+            LOGGER.info(f'Changed gid from {gid} to {new_gid}')
+        else:
+            if dl:
+                threading.Thread(target=dl.getListener().onDownloadComplete).start()
+
+    @new_thread
+    def __onDownloadStopped(self, api, gid):
+        sleep(0.5)
+        dl = getDownloadByGid(gid)
+        if dl: 
+            dl.getListener().onDownloadError('Dead torrent!')
+
+    @new_thread
+    def __onDownloadError(self, api, gid):
+        sleep(0.5)  # sleep for split second to ensure proper dl gid update from onDownloadComplete
+        dl = getDownloadByGid(gid)
+        download = api.get_download(gid)
+        error = download.error_message
+        LOGGER.info(f"Download Error: {error}")
+        if dl: 
+            dl.getListener().onDownloadError(error)
+
+    def start_listener(self):
+        aria2.listen_to_notifications(threaded=True, on_download_start=self.__onDownloadStarted,
+                                      on_download_error=self.__onDownloadError,
+                                      on_download_stop=self.__onDownloadStopped,
+                                      on_download_complete=self.__onDownloadComplete)
+
+    def add_download(self, link: str, path, listener, filename):
+        if is_magnet(link):
+            download = aria2.add_magnet(link, {'dir': path, 'out': filename})
+        else:
+            download = aria2.add_uris([link], {'dir': path, 'out': filename})
+        if download.error_message:  # no need to proceed further at this point
+            listener.onDownloadError(download.error_message)
+            return
+        with download_dict_lock:
+            download_dict[listener.uid] = AriaDownloadStatus(download.gid, listener)
+            LOGGER.info(f"Started: {download.gid} DIR:{download.dir} ")
+        self.listener = listener
---
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -0,0 +1,315 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Helper Module containing various sites direct links generators. This module is copied and modified as per need
+from https://github.com/AvinashReddy3108/PaperplaneExtended . I hereby take no credit of the following code other
+than the modifications. See https://github.com/AvinashReddy3108/PaperplaneExtended/commits/master/userbot/modules/direct_links.py
+for original authorship. """
+
+from bot import UPTOBOX_TOKEN
+import logging
+import json
+import math
+import re
+import urllib.parse
+from os import popen
+from random import choice
+from urllib.parse import urlparse
+
+import lk21
+import requests
+from bs4 import BeautifulSoup
+from js2py import EvalJs
+from lk21.extractors.bypasser import Bypass
+from base64 import standard_b64encode
+from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
+
+
+def direct_link_generator(link: str):
+    """ direct links generator """
+    if not link:
+        raise DirectDownloadLinkException("`No links found!`")
+    elif 'youtube.com' in link or 'youtu.be' in link:
+        raise DirectDownloadLinkException(f"Youtube Link use /{BotCommands.WatchCommand} or /{BotCommands.TarWatchCommand}")
+    elif 'zippyshare.com' in link:
+        return zippy_share(link)
+    elif 'yadi.sk' in link:
+        return yandex_disk(link)
+    elif 'cloud.mail.ru' in link:
+        return cm_ru(link)
+    elif 'mediafire.com' in link:
+        return mediafire(link)
+    elif 'uptobox.com' in link:
+        return uptobox(link)
+    elif 'osdn.net' in link:
+        return osdn(link)
+    elif 'github.com' in link:
+        return github(link)
+    elif 'hxfile.co' in link:
+        return hxfile(link)
+    elif 'anonfiles.com' in link:
+        return anon(link)
+    elif 'letsupload.io' in link:
+        return letsupload(link)
+    elif 'fembed.com' in link:
+        return fembed(link)
+    elif 'femax20.com' in link:
+        return fembed(link)
+    elif 'feurl.com' in link:
+        return fembed(link)
+    elif 'naniplay.nanime.in' in link:
+        return fembed(link)
+    elif 'naniplay.nanime.biz' in link:
+        return fembed(link)
+    elif 'naniplay.com' in link:
+        return fembed(link)
+    elif 'layarkacaxxi.icu' in link:
+        return fembed(link)
+    elif 'sbembed.com' in link:
+        return sbembed(link)
+    elif 'streamsb.net' in link:
+        return sbembed(link)
+    elif '1drv.ms' in link:
+        return onedrive(link)
+    elif 'pixeldrain.com' in link:
+        return pixeldrain(link)
+    else:
+        raise DirectDownloadLinkException(f'No Direct link function found for {link}')
+
+
+def zippy_share(url: str) -> str:
+    """ ZippyShare direct links generator
+    Based on https://github.com/KenHV/Mirror-Bot """
+    link = re.findall("https:/.(.*?).zippyshare", url)[0]
+    response_content = (requests.get(url)).content
+    bs_obj = BeautifulSoup(response_content, "lxml")
+
+    try:
+        js_script = bs_obj.find("div", {"class": "center",}).find_all(
+            "script"
+        )[1]
+    except:
+        js_script = bs_obj.find("div", {"class": "right",}).find_all(
+            "script"
+        )[0]
+
+    js_content = re.findall(r'\.href.=."/(.*?)";', str(js_script))
+    js_content = 'var x = "/' + js_content[0] + '"'
+
+    evaljs = EvalJs()
+    setattr(evaljs, "x", None)
+    evaljs.execute(js_content)
+    js_content = getattr(evaljs, "x")
+
+    return f"https://{link}.zippyshare.com{js_content}"
+
+
+def yandex_disk(url: str) -> str:
+    """ Yandex.Disk direct links generator
+    Based on https://github.com/wldhx/yadisk-direct """
+    try:
+        link = re.findall(r'\bhttps?://.*yadi\.sk\S+', url)[0]
+    except IndexError:
+        reply = "`No Yandex.Disk links found`\n"
+        return reply
+    api = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={}'
+    try:
+        dl_url = requests.get(api.format(link)).json()['href']
+        return dl_url
+    except KeyError:
+        raise DirectDownloadLinkException("`Error: File not found/Download limit reached`\n")
+
+
+def cm_ru(url: str) -> str:
+    """ cloud.mail.ru direct links generator
+    Using https://github.com/JrMasterModelBuilder/cmrudl.py """
+    reply = ''
+    try:
+        link = re.findall(r'\bhttps?://.*cloud\.mail\.ru\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No cloud.mail.ru links found`\n")
+    command = f'vendor/cmrudl.py/cmrudl -s {link}'
+    result = popen(command).read()
+    result = result.splitlines()[-1]
+    try:
+        data = json.loads(result)
+    except json.decoder.JSONDecodeError:
+        raise DirectDownloadLinkException("`Error: Can't extract the link`\n")
+    dl_url = data['download']
+    return dl_url
+
+
+def uptobox(url: str) -> str:
+    """ Uptobox direct links generator
+    based on https://github.com/jovanzers/WinTenCermin """
+    try:
+        link = re.findall(r'\bhttps?://.*uptobox\.com\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No Uptobox links found`\n")
+    if UPTOBOX_TOKEN is None:
+        logging.error('UPTOBOX_TOKEN not provided!')
+        dl_url = url
+    else:
+        try:
+            link = re.findall(r'\bhttp?://.*uptobox\.com/dl\S+', url)[0]
+            logging.info('Uptobox direct link')
+            dl_url = url
+        except:
+            file_id = re.findall(r'\bhttps?://.*uptobox\.com/(\w+)', url)[0]
+            file_link = 'https://uptobox.com/api/link?token=%s&file_code=%s' % (UPTOBOX_TOKEN, file_id)
+            req = requests.get(file_link)
+            result = req.json()
+            dl_url = result['data']['dlLink']
+    return dl_url
+
+
+def mediafire(url: str) -> str:
+    """ MediaFire direct links generator """
+    try:
+        link = re.findall(r'\bhttps?://.*mediafire\.com\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No MediaFire links found`\n")
+    page = BeautifulSoup(requests.get(link).content, 'lxml')
+    info = page.find('a', {'aria-label': 'Download file'})
+    dl_url = info.get('href')
+    return dl_url
+
+
+def osdn(url: str) -> str:
+    """ OSDN direct links generator """
+    osdn_link = 'https://osdn.net'
+    try:
+        link = re.findall(r'\bhttps?://.*osdn\.net\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No OSDN links found`\n")
+    page = BeautifulSoup(
+        requests.get(link, allow_redirects=True).content, 'lxml')
+    info = page.find('a', {'class': 'mirror_link'})
+    link = urllib.parse.unquote(osdn_link + info['href'])
+    mirrors = page.find('form', {'id': 'mirror-select-form'}).findAll('tr')
+    urls = []
+    for data in mirrors[1:]:
+        mirror = data.find('input')['value']
+        urls.append(re.sub(r'm=(.*)&f', f'm={mirror}&f', link))
+    return urls[0]
+
+
+def github(url: str) -> str:
+    """ GitHub direct links generator """
+    try:
+        re.findall(r'\bhttps?://.*github\.com.*releases\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No GitHub Releases links found`\n")
+    download = requests.get(url, stream=True, allow_redirects=False)
+    try:
+        dl_url = download.headers["location"]
+        return dl_url
+    except KeyError:
+        raise DirectDownloadLinkException("`Error: Can't extract the link`\n")
+
+
+def hxfile(url: str) -> str:
+    """ Hxfile direct links generator
+    based on https://github.com/breakdowns/slam-mirrorbot """
+    dl_url = ''
+    try:
+        link = re.findall(r'\bhttps?://.*hxfile\.co\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No Hxfile links found`\n")
+    bypasser = lk21.Bypass()
+    dl_url=bypasser.bypass_url(link)
+    return dl_url
+
+
+def anon(url: str) -> str:
+    """ Anonfiles direct links generator
+    based on https://github.com/breakdowns/slam-mirrorbot """
+    dl_url = ''
+    try:
+        link = re.findall(r'\bhttps?://.*anonfiles\.com\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No Anonfiles links found`\n")
+    bypasser = lk21.Bypass()
+    dl_url=bypasser.bypass_url(link)
+    return dl_url
+
+
+def letsupload(url: str) -> str:
+    """ Letsupload direct link generator
+    Based on https://github.com/breakdowns/slam-mirrorbot """
+    dl_url = ''
+    try:
+        link = re.findall(r'\bhttps?://.*letsupload\.io\S+', url)[0]
+    except IndexError:
+        raise DirectDownloadLinkException("`No Letsupload links found`\n")
+    bypasser = lk21.Bypass()
+    dl_url=bypasser.bypass_url(link)
+    return dl_url
+
+
+def fembed(link: str) -> str:
+    """ Fembed direct link generator
+    Based on https://github.com/breakdowns/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url=bypasser.bypass_fembed(link)
+    lst_link = []
+    count = len(dl_url)
+    for i in dl_url:
+        lst_link.append(dl_url[i])
+    return lst_link[count-1]
+
+
+def sbembed(link: str) -> str:
+    """ Sbembed direct link generator
+    Based on https://github.com/breakdowns/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url=bypasser.bypass_sbembed(link)
+    lst_link = []
+    count = len(dl_url)
+    for i in dl_url:
+        lst_link.append(dl_url[i])
+    return lst_link[count-1]
+
+
+def onedrive(link: str) -> str:
+    """ Onedrive direct link generator
+    Based on https://github.com/UsergeTeam/Userge """
+    link_without_query = urlparse(link)._replace(query=None).geturl()
+    direct_link_encoded = str(standard_b64encode(bytes(link_without_query, "utf-8")), "utf-8")
+    direct_link1 = f"https://api.onedrive.com/v1.0/shares/u!{direct_link_encoded}/root/content"
+    resp = requests.head(direct_link1)
+    if resp.status_code != 302:
+        return "`Error: Unauthorized link, the link may be private`"
+    dl_link = resp.next.url
+    file_name = dl_link.rsplit("/", 1)[1]
+    resp2 = requests.head(dl_link)
+    return dl_link
+
+
+def pixeldrain(url: str) -> str:
+    """ Based on https://github.com/yash-dk/TorToolkit-Telegram """
+    url = url.strip("/ ")
+    file_id = url.split("/")[-1]
+    info_link = f"https://pixeldrain.com/api/file/{file_id}/info"
+    dl_link = f"https://pixeldrain.com/api/file/{file_id}"
+    resp = requests.get(info_link).json()
+    if resp["success"]:
+        return dl_link
+    else:
+        raise DirectDownloadLinkException("ERROR: Cant't download due {}.".format(resp.text["value"]))
+
+
+def useragent():
+    """
+    useragent random setter
+    """
+    useragents = BeautifulSoup(
+        requests.get(
+            'https://developers.whatismybrowser.com/'
+            'useragents/explore/operating_system_name/android/').content,
+        'lxml').findAll('td', {'class': 'useragent'})
+    user_agent = choice(useragents)
+    return user_agent.text
