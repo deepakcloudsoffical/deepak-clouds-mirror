31b0df5c36a904e077a5b77252d7da402858576f
Deepak Clouds <76053688+deepakcloudsoffical@users.noreply.github.com>
2021-06-28 16:32:59 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -107,15 +107,15 @@ def get_readable_message():
                     msg += f"\n<b>😻Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 else:
                     msg += f"\n<b>Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>⚡️Speed:</b> {download.speed()}\n<b>ETA:</b> {download.eta()} "
+                msg += f"\n<b>⚡️Speed:</b> {download.speed()}\n<b>⏱ETA:</b> {download.eta()} "
                 # if hasattr(download, 'is_torrent'):
                 try:
-                    msg += f"\n<b>Seeders:</b> {download.aria_download().num_seeders}" \
-                        f" | <b>Peers:</b> {download.aria_download().connections}"
+                    msg += f"\n<b>🌾Seeders:</b> {download.aria_download().num_seeders}" \
+                        f" | <b>🥀Peers:</b> {download.aria_download().connections}"
                 except:
                     pass
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                msg += f"\n<b>To Stop:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += f"\n<b>To Stop🚫:</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             msg += "\n\n"
         return msg
 
