75e01cad06ee8ebeb3c48b033892587fa666d205
Deepak Nishad <76053688+blackdeep04@users.noreply.github.com>
2021-06-28 14:55:09 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -288,9 +288,9 @@ except KeyError:
 try:
     IMAGE_URL = getConfig('IMAGE_URL')
     if len(IMAGE_URL) == 0:
-        IMAGE_URL = 'https://telegra.ph/file/db03910496f06094f1f7a.jpg'
+        IMAGE_URL = 'https://imagetot.com/images/2021/06/28/e8155fe3d87a1084d7cda5f4ecc5211c.jpg'
 except KeyError:
-    IMAGE_URL = 'https://telegra.ph/file/db03910496f06094f1f7a.jpg'
+    IMAGE_URL = 'https://imagetot.com/images/2021/06/28/e8155fe3d87a1084d7cda5f4ecc5211c.jpg'
 
 IGNORE_PENDING_REQUESTS = False
 try:
